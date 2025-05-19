11a1ea3ffe034c1c1d60a4eaf70d6a35cb6607c5
Nilanjan Mondal <nilanjanmondal165@gmail.com>
2025-05-19 22:58:05 +0530

    [CHORE] : Basic server setup with database connection

---
+++ server/src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const serverConfig = require('./configs/serverConfig');
+const connectDB = require('./configs/dbConfig');
+
+
+const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173", // Replace with your client's origin for localhost
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add the methods you need
+    credentials: true // Allow credentials (cookies, authorization headers, etc.)
+}));
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    return res.json({
+        message: "Welcome to Recovaid..."
+    })
+})
+
+//server starts here
+app.listen(serverConfig.PORT, async () => {
+    await connectDB();
+    console.log(`Server started at port ${serverConfig.PORT}...!!`);
+});
+
+
+
