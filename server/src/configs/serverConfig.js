59e2c99e7e2a5ceafac7f011028f1b13242d3026
Nilanjan Mondal <nilanjanmondal165@gmail.com>
2025-05-20 01:37:00 +0530

    [CHORE] : Add JWT_SECRET to server configuration for token signing

--- server/src/configs/serverConfig.js
+++ server/src/configs/serverConfig.js
@@ -4,5 +4,6 @@ dotenv.config();
 // Here we are exporting all the env variables that the project uses
 module.exports = {
     PORT: process.env.PORT,
-    DB_URL: process.env.DB_URL
+    DB_URL: process.env.DB_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
 }
