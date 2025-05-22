7418e2d20ffbd8c9f27786a7eb45f7a70dcefc49
Nilanjan Mondal <nilanjanmondal165@gmail.com>
2025-05-23 01:12:05 +0530

    [FEAT] : Add authenticated API for doctor's response to patient's status

--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser')
 const userRoutes = require("./routes/user.routes");
 const authRoutes = require("./routes/auth.routes");
 const dailyStatusRoutes = require("./routes/dailyStatus.routes");
+const respondToStatusRoutes = require("./routes/respondToStatus.routes");
 
 
 const app = express();
@@ -32,6 +33,7 @@ app.get('/', (req, res) => {
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/status", dailyStatusRoutes);
+app.use("/api/status/respond", respondToStatusRoutes);
 
 //server starts here
 app.listen(serverConfig.PORT, async () => {
