5f39f01ba052fb2ad2a63c113d80a0dfeb853872
Nilanjan Mondal <nilanjanmondal165@gmail.com>
2025-05-28 13:34:41 +0530

    [FEAT] : Add conditional rendering of Auth Card

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,31 @@ import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
+import Auth from './components/Auth'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [authOpen, setAuthOpen] = useState(false);
 
   return (
     <>
-      <Navbar />
-      <Home />
+
+      {/* React Component Data Flow ->
+      Data (props) flows from Parent → Child
+      Always one-way: top-down (parent to child)
+
+      A parent sends data to a child using props.
+
+      The child cannot directly modify the parent’s state — it can only ask the parent to do something (e.g., by calling a function passed as a prop). 
+      
+      Here App.jsx is the parent component and Navbar is the child component.
+      The Navbar component receives the onRegisterClick function as a prop, which it can call when the "Register Now" button is clicked.
+      The App component manages the state of whether the Auth modal is open or not, and it passes a function to Navbar that allows it to open the Auth modal when the button is clicked.
+      */}
+
+
+      <Navbar onRegisterClick={() => setAuthOpen(true)} />
+      {authOpen && (<Auth onClose={() => setAuthOpen(false)} />)}
+      <Home onRegisterClick={() => setAuthOpen(true)}/>
     </>
   )
 }
