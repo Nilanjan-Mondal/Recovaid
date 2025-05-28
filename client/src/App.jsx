f8a54a00ad2d7b15ae980c8b5be69e59281db00b
Nilanjan Mondal <nilanjanmondal165@gmail.com>
2025-05-28 13:41:35 +0530

    [REFACTOR] : Update comments on react's data flow and re rendering components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,17 +10,21 @@ function App() {
   return (
     <>
 
-      {/* React Component Data Flow ->
-      Data (props) flows from Parent → Child
-      Always one-way: top-down (parent to child)
+      {/* 
+        React Component Data Flow ->
+        Data (props) flows from Parent → Child
+        Always one-way: top-down (parent to child)
 
-      A parent sends data to a child using props.
+        A parent sends data to a child using props.
 
-      The child cannot directly modify the parent’s state — it can only ask the parent to do something (e.g., by calling a function passed as a prop). 
-      
-      Here App.jsx is the parent component and Navbar is the child component.
-      The Navbar component receives the onRegisterClick function as a prop, which it can call when the "Register Now" button is clicked.
-      The App component manages the state of whether the Auth modal is open or not, and it passes a function to Navbar that allows it to open the Auth modal when the button is clicked.
+        The child cannot directly modify the parent’s state — it can only ask the parent to do something (e.g., by calling a function passed as a prop). 
+        
+        Here App.jsx is the parent component and Navbar is the child component.
+        The Navbar component receives the onRegisterClick function as a prop, which it can call when the "Register Now" button is clicked.
+        
+        Auth component is conditionally rendered based on the authOpen state, which is controlled by the App component.
+
+        everytime the state is changed react re-renders the app component completely thereby achieving the desired effect of showing and hiding the Auth component.
       */}
 
 
