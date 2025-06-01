d71ddf7e7cf7f05a98301ce2d6d394d348f24a15
Nilanjan Mondal <nilanjanmondal165@gmail.com>
2025-06-01 20:55:09 +0530

    [FEAT] : Implement mark as read on doctor's reply functionality

--- server/src/repositories/respondToStatus.repository.js
+++ server/src/repositories/respondToStatus.repository.js
@@ -1,9 +1,10 @@
 const DailyStatus = require("../schemas/dailyStatusSchema");
-
 async function updateDoctorResponse(statusId, doctorId, response) {
   const result =  await DailyStatus.findOneAndUpdate(
     { _id: statusId, doctor: doctorId },
-    { doctorResponse: response },
+    { doctorResponse: response,
+      read: true
+    },
     { new: true } // this helps in returning the updated object immediately without this we would get the old object in result
   );
 
